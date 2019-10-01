33f6f079af6ef848e37d5fade36a5418aee6388a
Katniss Lee <jihyunlee@berkeley.edu>
2019-09-30 23:25:01 -0700

    part 1 passes

--- colorPalette.c
+++ colorPalette.c
@@ -20,14 +20,90 @@ void usage(char* argv[])
 int P3colorpalette(char* colorfile, int width, int heightpercolor, char* outputfile)
 {
 	//YOUR CODE HERE
-	return 0;
+	FILE* input = fopen(colorfile, "r");
+	FILE* output = fopen(outputfile, "w"); 
+	int length_of_colorfile = 0;
+	fscanf(input, "%d", &length_of_colorfile);
+	if (!length_of_colorfile || heightpercolor < 1) {
+	    return 1; 
+	}
+        int vertical_dim = heightpercolor * (length_of_colorfile);  
+
+	//let's first print the first line of ppm file
+	fprintf(output, "%s %d %d %d\n", "P3", width, vertical_dim, 255); 
+	
+	//now we will loop through the rest to complete this 
+	int* colorcount = (int*) malloc(sizeof(int*)); 
+	uint8_t** color_array = FileToColorMap(colorfile, colorcount);
+	
+	//make sure the colorfile is valid and if it is not, return 1
+	if (color_array == NULL) {
+	    return 1; 
+	} 
+
+	for (int i = 0; i < length_of_colorfile; i++) {
+	    uint8_t one, two, three; 
+	    one = color_array[i][0];
+	    two = color_array[i][1];
+	    three = color_array[i][2];
+	    //make sure single line of color is replicated as much as width
+	    for (int j = 0; j < heightpercolor; j++) {
+	        int count = 0; 
+		while (count < width) {
+		//one starts in new line if the count is zero
+			if (count != width - 1) {
+			 fprintf(output, "%d %d %d ", one, two, three); 
+		    } else {
+		         fprintf(output, "%d %d %d\n", one, two, three); 
+	            }
+	           count++;
+	        }     
+	    }
+        }	    
+    free(color_array);
+    fclose(input);
+    fclose(output);
+    return 0;
 }
 
 //Same as above, but with P6 format
 int P6colorpalette(char* colorfile, int width, int heightpercolor, char* outputfile)
 {
-	//YOUR CODE HERE
-	return 0;
+    //YOUR CODE HERE
+    FILE* input = fopen(colorfile, "r");
+    FILE* output = fopen(outputfile, "w");
+    int length_of_colorfile = 0;
+    fscanf(input, "%d", &length_of_colorfile);
+    if (!length_of_colorfile || heightpercolor < 1) {
+        return 1;
+    }
+    int vertical_dim = heightpercolor * (length_of_colorfile);
+
+    //let's first print the first line of ppm file
+    fprintf(output, "%s %d %d %d\n", "P6", width, vertical_dim, 255);
+	
+    //now we will loop through the rest to complete this 
+        int* colorcount = (int*) malloc(sizeof(int*));
+        uint8_t** color_array = FileToColorMap(colorfile, colorcount);
+
+        //make sure the colorfile is valid and if it is not, return 1
+        if (color_array == NULL) {
+            return 1;
+        }
+
+        for (int i = 0; i < length_of_colorfile; i++) {
+            //make sure single line of color is replicated as much as width
+            int count = 0;
+            while (count < width) {
+            //one starts in new line if the count is zero
+               fprintf(output, "%c", );
+               count++;
+            }
+        }
+    free(color_array);
+    fclose(input);
+    fclose(output);
+    return 0;
 }
 
 
